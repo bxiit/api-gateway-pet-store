7cbfb4046bfe73f90a4e267011525cdb1c0a1eab
bxit <arturivanov50808@gmail.com>
2024-05-30 05:47:42 +0500

    initial commit

---
+++ go.mod
@@ -0,0 +1,21 @@
+module api-gateway
+
+go 1.21
+
+require (
+	github.com/bxiit/protos v0.0.12
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.20.0
+	github.com/rabbitmq/amqp091-go v1.10.0
+	google.golang.org/genproto/googleapis/api v0.0.0-20240513163218-0867130af1f8
+	google.golang.org/grpc v1.64.0
+	google.golang.org/protobuf v1.34.1
+)
+
+require (
+	github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
+	github.com/rs/cors v1.11.0 // indirect
+	golang.org/x/net v0.25.0 // indirect
+	golang.org/x/sys v0.20.0 // indirect
+	golang.org/x/text v0.15.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240521202816-d264139d666e // indirect
+)
---
+++ sender/send.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"context"
+	amqp "github.com/rabbitmq/amqp091-go"
+	"log"
+	"time"
+)
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "failed to connect to rabbit")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"hello2", // name
+		false,    // durable
+		false,    // delete when unused
+		false,    // exclusive
+		false,    // no-wait
+		nil,      // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	ctx, cancelFunc := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancelFunc()
+
+	body := "Hello World!"
+
+	err = ch.PublishWithContext(
+		ctx,
+		"",
+		q.Name,
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	failOnError(err, "failed to publish a message")
+	log.Printf("[x] Sent %s\n", body)
+
+}
+
+func failOnError(e error, msg string) {
+	if e != nil {
+		log.Panicf("%s: %v", msg, e)
+	}
+}
